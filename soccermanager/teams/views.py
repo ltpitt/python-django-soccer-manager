59db63b3f51ed696834a4cc0ab40b746d7b73ec7
Davide Nastri <d.nastri@gmail.com>
2019-05-18 14:16:25 +0200

    feat: Added logout to teams views

--- soccermanager/teams/views.py
+++ soccermanager/teams/views.py
@@ -4,4 +4,4 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def index(request):
-    return HttpResponse("Welcome to Soccer Manager Teams page")
+    return HttpResponse("Welcome to Soccer Manager Teams page <a href=\"/accounts/logout\">Logout</a>")
